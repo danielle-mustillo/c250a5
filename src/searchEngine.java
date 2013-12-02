2c915121db121fee022fc7a417cc723ce60d827e
Danielle Mustillo <danielle.mustillo@mail.mcgill.ca>
2013-12-02 00:35:38 -0500

    Assignment complete.

--- src/searchEngine.java
+++ src/searchEngine.java
@@ -1,3 +1,6 @@
+// STUDENT_NAME: Danielle Mustillo
+// STUDENT_ID: 260533476
+
 import java.util.*;
 import java.io.*;
 
@@ -31,6 +34,37 @@ public class searchEngine {
     
     void traverseInternet(String url) throws Exception {
 	/* WRITE SOME CODE HERE */
+    	//set visited and page rank
+    	internet.setVisited(url, true);
+    	internet.setPageRank(url, 1);
+    	
+    	//add the words to the wordIndex
+    	LinkedList<String> words = htmlParsing.getContent(url);
+    	
+    	//add words to wordIndex, also add the website to that words list of URL's. 
+    	for(String word : words)
+    		if(!wordIndex.containsKey(word)) {
+    			LinkedList<String> newWord = new LinkedList<String>();
+    			newWord.addLast(url);
+    			wordIndex.put(word, newWord);
+    		}
+    		else {
+    			LinkedList<String> theURLs = wordIndex.get(word);
+    			theURLs.addLast(url);
+    		}
+    	
+    	/* Go to each URL in the current webpage, 
+    	 * Add it to our internet directedGraph.
+    	 */
+    	LinkedList<String> outgoingURL = htmlParsing.getLinks(url);
+    	Iterator<String> iterator = outgoingURL.iterator();
+    	while(iterator.hasNext()) {
+    		String next = iterator.next();
+    		internet.addEdge(url, next);
+    		if(!internet.getVisited(next)) {
+    			traverseInternet(next);
+    		}
+    	}
 	
 	/* Hints
 	   0) This should take about 50-70 lines of code (or less)
@@ -65,7 +99,32 @@ public class searchEngine {
     */
     void computePageRanks() {
 	/* WRITE YOUR CODE HERE */
-	
+    	
+    	
+    	for(int resolution = 0; resolution < 10; resolution++) {
+    		Iterator<String> urls = internet.vertices.keySet().iterator();
+    		
+    		//store the page ranks temporarily to add at end of iteration
+    		HashMap<String, double[]> newMap = new HashMap<String, double[]>();
+    		
+        	while(urls.hasNext()) {
+    			String url = urls.next();
+    			double pageRank[] = {0.5};
+    			LinkedList<String> references = internet.getEdgesInto(url);
+    			for(String reference : references) {
+    				double rank = internet.getPageRank(reference);
+    				int outDegree = internet.getOutDegree(reference);
+    				pageRank[0] += rank / outDegree * 0.5;
+    			}
+    			newMap.put(url, pageRank);
+    		}
+        	
+        	Iterator<String> url2 = newMap.keySet().iterator();
+        	while(url2.hasNext()) {
+        		String url = url2.next();
+        		internet.setPageRank(url, newMap.get(url)[0]);
+        	}
+    	}
     } // end of computePageRanks
     
 	
@@ -78,25 +137,47 @@ public class searchEngine {
     */
     String getBestURL(String query) {
 	/* WRITE YOUR CODE HERE */
+    	LinkedList<String> sites = new LinkedList<String>();
+    	do {
+    		int space = query.indexOf(' ') > -1 ? query.indexOf(' ') : query.length();
+    		String word = query.substring(0, space);
+    		
+    		if(space != query.length())
+    			query = query.substring(space + 1);
+    		else
+    			query = "";
+    		
+    		if(wordIndex.containsKey(word)) {
+    			LinkedList<String> urls = wordIndex.get(word); 
+    			for(String url : urls) {
+    				sites.addLast(url);
+    			}
+    		}
+    		query = query.substring(query.indexOf(' ') + 1);
+    	}while(query.length() > 0);
 
-	return null; // remove this
+		String bestSiteSofar = "";
+		for(String url : sites)
+			if(internet.getPageRank(url) > internet.getPageRank(bestSiteSofar))
+				bestSiteSofar = url;
+  
+		return bestSiteSofar; // remove this
     } // end of getBestURL
-    
     
 	
     public static void main(String args[]) throws Exception{		
 		searchEngine mySearchEngine = new searchEngine();
 		// to debug your program, start with.
-		// mySearchEngine.traverseInternet("http://www.cs.mcgill.ca/~blanchem/250/a.html");
+//		 mySearchEngine.traverseInternet("http://www.cs.mcgill.ca/~blanchem/250/a.html");
 
 		// When your program is working on the small example, move on to
 		mySearchEngine.traverseInternet("http://www.cs.mcgill.ca");
 
 		// TODO this is just for debugging purposes. REMOVE THIS BEFORE
 		// SUBMITTING
-		System.out.println(mySearchEngine);
-
 		mySearchEngine.computePageRanks();
+		
+//		System.out.println(mySearchEngine);
 
 		BufferedReader stndin = new BufferedReader(new InputStreamReader(
 				System.in));
@@ -106,7 +187,7 @@ public class searchEngine {
 			query = stndin.readLine();
 			if (query != null && query.length() > 0) {
 				System.out.println("Best site = "
-						+ mySearchEngine.getBestURL(query));
+						+ mySearchEngine.getBestURL(query) + " " + mySearchEngine.internet.getPageRank(mySearchEngine.getBestURL(query)));
 			}
 		} while (query != null && query.length() > 0);
 	} // end of main
