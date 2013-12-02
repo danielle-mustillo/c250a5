3245808ba66b0427c631d65ebc830dcca0d9328d
Danielle Mustillo <danielle.mustillo@mail.mcgill.ca>
2013-12-02 00:36:28 -0500

    Removed comments that are no longer relevant.

--- src/searchEngine.java
+++ src/searchEngine.java
@@ -66,41 +66,12 @@ public class searchEngine {
     		}
     	}
 	
-	/* Hints
-	   0) This should take about 50-70 lines of code (or less)
-	   1) To parse the content of the url, call
-	   htmlParsing.getContent(url), which returns a LinkedList of Strings 
-	   containing all the words at the given url. Also call htmlParsing.getLinks(url).
-	   and assign their results to a LinkedList of Strings.
-	   2) To iterate over all elements of a LinkedList, use an Iterator,
-	   as described in the text of the assignment
-	   3) Refer to the description of the LinkedList methods at
-	   http://docs.oracle.com/javase/6/docs/api/ .
-	   You will most likely need to use the methods contains(String s), 
-	   addLast(String s), iterator()
-	   4) Refer to the description of the HashMap methods at
-	   http://docs.oracle.com/javase/6/docs/api/ .
-	   You will most likely need to use the methods containsKey(String s), 
-	   get(String s), put(String s, LinkedList l).  
-	*/
-	
 	
 	
     } // end of traverseInternet
     
-    
-    /* This computes the pageRanks for every vertex in the internet graph.
-       It will only be called after the internet graph has been constructed using 
-       traverseInternet.
-       Use the iterative procedure described in the text of the assignment to
-       compute the pageRanks for every vertices in the graph. 
-       
-       This method will probably fit in about 30 lines.
-    */
+ 
     void computePageRanks() {
-	/* WRITE YOUR CODE HERE */
-    	
-    	
     	for(int resolution = 0; resolution < 10; resolution++) {
     		Iterator<String> urls = internet.vertices.keySet().iterator();
     		
@@ -127,14 +98,6 @@ public class searchEngine {
     	}
     } // end of computePageRanks
     
-	
-    /* Returns the URL of the page with the high page-rank containing the query word
-       Returns the String "" if no web site contains the query.
-       This method can only be called after the computePageRanks method has been executed.
-       Start by obtaining the list of URLs containing the query word. Then return the URL 
-       with the highest pageRank.
-       This method should take about 25 lines of code.
-    */
     String getBestURL(String query) {
 	/* WRITE YOUR CODE HERE */
     	LinkedList<String> sites = new LinkedList<String>();
@@ -167,17 +130,8 @@ public class searchEngine {
 	
     public static void main(String args[]) throws Exception{		
 		searchEngine mySearchEngine = new searchEngine();
-		// to debug your program, start with.
-//		 mySearchEngine.traverseInternet("http://www.cs.mcgill.ca/~blanchem/250/a.html");
-
-		// When your program is working on the small example, move on to
 		mySearchEngine.traverseInternet("http://www.cs.mcgill.ca");
-
-		// TODO this is just for debugging purposes. REMOVE THIS BEFORE
-		// SUBMITTING
 		mySearchEngine.computePageRanks();
-		
-//		System.out.println(mySearchEngine);
 
 		BufferedReader stndin = new BufferedReader(new InputStreamReader(
 				System.in));
